01a2c6f7427e53427c370e036fe950c9b173d853
Halospace Foundation <contact@halospace.org>
2022-01-06 17:28:30 -0800

    db: also init user_addr_tokn

--- hybot/data/__init__.py
+++ hybot/data/__init__.py
@@ -5,6 +5,7 @@ from .addr import *
 from .tokn import *
 from .user_addr import *
 from .user_tokn import *
+from .user_addr_tokn import *
 from .db import *
 
-del base, user, addr, tokn, user_addr, user_tokn, db
+del base, user, addr, tokn, user_addr, user_tokn, user_addr_tokn, db
