8e698fbdcd4fce39421174a650a490cc80a0c9cd
Halospace Foundation <contact@halospace.org>
2022-01-06 18:40:35 -0800

    bot: initial db interfacing works!

--- hybot/bot/hydra.py
+++ hybot/bot/hydra.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 from aiogram import Bot, Dispatcher, types
+from aiogram.types.user import User as TelegramUser
 from attrdict import AttrDict
+import asyncio
 
 from hydra.rpc import HydraRPC
 
+from ..data import *
 from ..conf import Config
 
 CONF = {
@@ -12,27 +15,25 @@ CONF = {
 }
 
 
-def singletonmethod(func):
-    return lambda *a, **k: HydraBot.call(func, *a, **k)
-
-
 @Config.defaults(CONF)
 class HydraBot(Bot):
-    __HydraBot: HydraBot = None
+    _: HydraBot = None
     __DP = Dispatcher()
 
     rpc: HydraRPC = None
     conf: AttrDict = None
+    db: DB = None
 
     def __new__(cls, *args, **kwargs):
-        if cls.__HydraBot is None:
-            cls.__HydraBot = super(HydraBot, cls).__new__(cls, *args, **kwargs)
+        if cls._ is None:
+            cls._ = super(HydraBot, cls).__new__(cls, *args, **kwargs)
 
-        return cls.__HydraBot
+        return cls._
 
     def __init__(self, rpc: HydraRPC):
         self.rpc = rpc
         self.conf = Config.get(HydraBot)
+        self.db = DB.default()
 
         token = self.conf.get("token", None)
 
@@ -42,18 +43,23 @@ class HydraBot(Bot):
         super().__init__(token, parse_mode="HTML")
 
     @staticmethod
-    def call(func, *args, **kwds):
-        return func(HydraBot.__HydraBot, *args, **kwds)
-
-    @staticmethod
     def main(bot: HydraBot):
         return bot.run()
 
     def run(self):
         return HydraBot.__DP.run_polling(self)
 
+    def db_get_user(self, tg_user: TelegramUser):
+        return self.db.user_load_or_create(tg_user)
+
+    async def db_get_user_async(self, tg_user: TelegramUser):
+        return await asyncio.get_event_loop().run_in_executor(None, self.db_get_user, tg_user)
+
     @staticmethod
     @__DP.message(commands={"hello"})
     async def hello(msg: types.Message):
-        await msg.answer(f"Hello, <b>{msg.from_user.full_name}!</b>")
+        user_data = await HydraBot._.db_get_user_async(msg.from_user)
+
+        await msg.answer(f"Hello, <b>{msg.from_user.full_name}!</b> Your HydraBot user id is #{user_data.user_id}")
+
 
