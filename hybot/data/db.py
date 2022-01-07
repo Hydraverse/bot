29b45bb8171c1418d32591d256c5cf76f2229669
Halospace Foundation <contact@halospace.org>
2022-01-06 18:38:20 -0800

    db: proper session handling

--- hybot/data/db.py
+++ hybot/data/db.py
@@ -1,6 +1,12 @@
 import os
+from typing import Optional
+
+import sqlalchemy.exc
+from attrdict import AttrDict
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
+from aiogram.types.user import User as TelegramUser
 
 from . import *
 from hydra import log
@@ -10,7 +16,6 @@ __all__ = "DB",
 
 class DB:
     engine = None
-    session = None
     Session = None
 
     FILE_DIR = "local"
@@ -20,14 +25,38 @@ class DB:
     def __init__(self, url, *args, **kwds):
         log.debug(f"db: open url='{url}'")
         self.engine = create_engine(url, *args, **kwds)
-        self.Session = sessionmaker(bind=self.engine)
-        self.session = self.Session()
+        self.Session = scoped_session(sessionmaker(bind=self.engine))
         Base.metadata.create_all(self.engine)
 
     @staticmethod
     def default():
         return DB(f"sqlite:///{DB.PATH}", echo=log.level() <= log.INFO)
 
-    def user_from_tgid(self, tgid) -> User:
-        return self.session.execute(User.from_tgid(tgid)).scalar_one()
+    def user_load_or_create(self, tg_user: TelegramUser) -> User:
+        self.Session()
 
+        try:
+            # noinspection PyProtectedMember
+            return self.Session.execute(User.from_tgid(tg_user.id)).scalar_one()
+
+        except sqlalchemy.exc.NoResultFound:
+            self.Session.rollback()
+
+            user_ = User(
+                tgid=tg_user.id,
+                name=tg_user.username,
+            )
+
+            # noinspection PyBroadException
+            try:
+                self.Session.add(user_)
+                self.Session.commit()
+
+            except BaseException:
+                self.Session.rollback()
+                raise
+
+            return user_
+
+        finally:
+            self.Session.remove()
