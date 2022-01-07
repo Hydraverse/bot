50ce7d311f96664736281d9d20618f7e8626471d
Halospace Foundation <contact@halospace.org>
2022-01-06 21:17:55 -0800

    bot: /nick command, /hello command complete

--- hybot/bot/hydra.py
+++ hybot/bot/hydra.py
@@ -52,8 +52,66 @@ class HydraBot(Bot):
     @staticmethod
     @__DP.message(commands={"hello"})
     async def hello(msg: types.Message):
-        user_data = await HydraBot._.db.user_load_or_create(msg.from_user)
+        u = await HydraBot._.db.user_load_or_create(msg.from_user)
 
-        await msg.answer(f"Hello, <b>{msg.from_user.full_name}!</b> Your HydraBot user id is #{user_data.user_id}")
+        nick = u.info.get("nick", None) or msg.from_user.username
 
+        response_nick_change = (
+            "Change your nickname with /nick [name]\n\n"
+        )
 
+        response_nick_is = (
+            f"Your nickname is {nick}.\n" +
+            response_nick_change
+        )
+
+        response_uid = (
+            f"Your HydraBot user id is #{u.user_id}.\n\n"
+        )
+
+        response_donate = (
+            "Please consider supporting the project developer.\n"
+            "Thank You!!\n\n"
+            f"{HydraBot._.conf.donations}")
+
+        if getattr(u.info, "welcomed", False) is not True:
+            await msg.answer(
+                f"Welcome, <b>{msg.from_user.full_name}!</b>\n\n" +
+                response_uid +
+                response_nick_is +
+                response_donate
+            )
+
+            await HydraBot._.db.user_info_update(msg.from_user, {
+                "welcomed": True,
+                "lang": msg.from_user.language_code,
+                "tz": "UTC",
+                "nick": nick,
+            })
+
+        else:
+            await msg.answer(
+                f"Hiya, <b>{u.info.nick}!</b>\n\n" +
+                response_uid +
+                response_nick_change +
+                response_donate
+            )
+
+    @staticmethod
+    @__DP.message(commands={"nick"})
+    async def hello(msg: types.Message):
+        u = await HydraBot._.db.user_load_or_create(msg.from_user)
+
+        nick_cur = u.info.get("nick", None)
+        nick_new = str(msg.text).replace("/nick", "").strip()
+
+        if not nick_new:
+            return await msg.answer(
+                f"Hiya, <b>{nick_cur}</b>!\n\n"
+                "Change your nickname with /nick [name]"
+            )
+
+        if nick_new == nick_cur:
+            await msg.answer(f"That's your nickname already, silly!\n\n")
+
+        await msg.answer(f"Nickname changed to <b>{nick}</b>\n\n")
