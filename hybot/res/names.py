6fb8aeece82f9b69bb0ae04b97bbef400eb79b56
Halospace Foundation <contact@halospace.org>
2022-01-09 08:52:12 -0800

    namegen: initial script

---
+++ hybot/res/names.py
@@ -0,0 +1,745 @@
+NAMES = """
+Alpha
+Acamar
+Achrady
+Adarak
+Adelphous
+Adigeon Prime
+Agaron
+Aia
+Ajilon Prime
+Akritiri
+Alastria
+Aldea
+Aldebaran
+Algeron
+Alonis
+Alpha Carinae
+Alpha Cygnus
+Alpha Eridani
+Alpha
+Alpha Majoris
+Alpha Moon
+Alpha Omicron
+Alpha Onias
+Altair
+Altamid
+Altec
+Alture
+Amleth Prime
+Andevian
+Andor
+Andoria
+Andros
+Angel
+Angosia
+Antede
+Antica
+Antos
+Arakis Prime
+Archanis
+Archer
+Arcturus
+Arcybite
+Ardana
+Argala
+Argana
+Argelius
+Argratha
+Argus
+Ariannus
+Arkaria
+Arloff
+Armus
+Arreta
+Arvada
+Aschelan
+Atalia
+Atbar Prime
+Athos
+Atifs
+Atrea
+audet
+Avenal
+Avery
+Axanar
+Azati Prime
+B'Saari
+Ba'ku
+Babel
+Bajor
+Bajoran Lunar
+Balancar
+Balosnee
+Banea
+Barisa Prime
+Barkon
+Barolia
+Barradas
+Barson
+Barzan
+Beltain
+Benecia
+Benthos
+Benzar
+Berengaria
+Bersallis
+Beta Agni
+Beta Antares
+Beta
+Beta Kupsic
+Beta Lankal
+Beta Moon
+Beta Rigel
+Beta Thoridar
+Beta Thoridor
+Betazed
+Beth Delta
+Bilana
+Blue Horizon
+Boam
+Bokara
+Bolarus
+Bolias
+Bopak
+Boraal
+Boradis
+Boranis
+Boreal
+Boreth
+Borzha
+Bracas
+Braslota
+Brax
+Bre'el
+Brekka
+Brentalia
+Brinda
+Bringloid
+Browder
+Bryma
+Burala prime
+Bynaus
+Caere
+Cairn
+Calder
+Caldik Prime
+Caldos
+Caleb
+Callinon
+Calondia
+Camor
+Campor
+Camus
+Canopus
+Capella
+Cardassia
+Carema
+Carraya
+Casperia Prime
+Castal
+Castor
+Catualla
+Celes
+Celtris
+Cerberus
+Cestus
+Ceti Alpha
+Chalna
+Chaltok
+Chand
+Chandra
+Chantil
+Chaya
+Cheron
+Chrysalia
+Cirrus
+Coltar
+Coppelius
+Cor Caroli
+Coridan
+Corinth
+Corvan
+Cygnet
+Daimanta
+Dakala
+Daled
+Danula
+Daran
+Davlos
+Dayos
+Decos Prime
+Dedestris
+Deinonychus
+Dekendi
+Delb
+Delinia
+Delios
+Delos
+Delta
+Delta Rana
+Delta Vega
+Delta Vega
+Deneb
+Deneva
+Denius
+Denobula
+Deriben
+Derna
+Dessica
+Devidia
+Devnar
+Devos
+Dimorus
+Donatu
+Doraf
+Dorala
+Dorvan
+Dozaria
+Draken
+Drayan
+Draygo
+Draylax
+Draylon
+Drema
+Dreon
+Droplet
+Dulisian
+Durenia
+Dyson Sphere
+Dytallix
+Earth
+Eden
+Ekos
+El-Adrel
+Elanu
+Elas
+Elba
+Emila
+Eminiar
+Enara
+Enara Prime
+Ennan
+Entelior
+Epsilon Canaris
+Epsilon Hydra
+Epsilon
+Epsilon Nel
+Erabus Prime
+Erilon
+Errikang
+Evadne
+Excalbia
+Exo
+Fahleena
+Farius Prime
+Felton Prime
+Fendaus
+Fenris
+Ferenginar
+Fina Prime
+Finnea Prime
+Folnar
+Forcas
+Forlat
+Freecloud
+Gagarin
+Gaia
+Galador
+Galdonterre
+Galen
+Gallos
+Galor
+Galorda Prime
+Galorndon Core
+Galvin
+Gamelan
+Gamma Hromi
+Gamma Hydra
+Gamma
+Gamma Tauri
+Gamma Trianguli
+Gamma Vertis
+Ganalda
+Garadius
+Garon
+Gaspar
+Gault
+Gedi Prime
+Gema
+Gemaris
+Gemulon
+Genesis
+Getheon
+Ghorusda
+Gideon
+Golana
+Golos Prime
+Gonal
+Gothos
+H'atoria
+Ha'Dara
+Hakton
+Halii
+Halkan
+Hanon
+Hanoran
+Harod
+Harrakis
+Haven
+Hekaras
+Hell
+Hemikek
+Hemikek
+Heva
+Hoek
+Holna
+Hur'Q
+Hurada
+Hurkos
+Hyralan
+Iadara
+Iconia
+Icor
+Ilari
+Ilidaria
+Inavar Prime
+Indri
+Inferna Prime
+Ingraham
+Invernia
+Itamish
+Ivor Prime
+Iyar
+Janus
+Japori
+Jaros
+Jemonon
+Jeraddo
+Jinoteur
+Jouret
+Juhraya
+Jupiter
+Kabrel
+Kabrel
+Kaelon
+Kalandan
+Kaldra
+Kalla
+Kanda
+Kantare
+Kataan
+Kavis Alpha
+Kea
+Kelton
+Kelva
+Kelvas
+Kenda
+Kentanna
+Kesprytt
+Kessik
+Keto Enol
+Khefka
+Khitomer
+Khofla
+Kilosa
+Klaestron
+Klavdia
+Kolarus
+Kora
+Korinar
+Korma
+Korris
+Korvat
+Kostolain
+Kotara Barath
+Kotati
+Kraus
+Kreetassa
+Krios
+Krios Prime
+Ktaria
+Ktarus
+Kurill Prime
+Kurl
+Kyana Prime
+Kylata
+La'voti
+Lambda
+Lamenda Prime
+Landris
+Lappa
+Largo
+Lavinius
+Lazon
+Ledonia
+Ledos
+Legara
+Lemma
+Lerais
+Lerishi
+Levinius
+Ligobis
+Ligon
+Ligos
+Lissepia
+Loracus Prime
+Loren
+Lorillia
+Loval
+Lumbu
+Lunar
+Lya
+Lysia
+Maarek
+Makus
+Malaya
+Malcor
+Malindi
+Malkus
+Malon Prime
+Manark
+Manu
+Maranga
+Marcos
+Marejaretus
+Mariah
+Marijne
+Mariposa
+Marlonia
+Mars
+Marva
+Matalas
+Matalas Prime
+Mataline
+Mavala
+Mazar
+Medusa
+Meezan
+Meldrar
+Meles
+Melina
+Melkot
+Melnos
+Melona
+Merak
+Mercury
+Meridian
+Merik
+Midos
+Mikah
+Milika
+Mimas
+Minara
+Minos
+Minos Korva
+Mintaka
+Mira
+Mirdonyae
+Miridian
+Mislan
+Mithren
+Mizar
+Moab
+Monac
+Moon
+Mordan
+Morikin
+Morska
+Mudd
+Mudor
+Myrmidon
+Nahmi
+Napinne
+Narendra
+Narva
+Nasreldine
+Nausicaa
+Nel
+Nelvana
+Nepenthe
+Nequencia Alpha
+Nervala
+Neural
+New Bajor
+New Earth
+New Gaul
+New Halana
+New Sausalito
+New Sydney
+New Vulcan
+Nibiru
+Nigala
+Nimbus
+Ninipia Prime
+Nivoch
+No'nat
+Norcadia Prime
+Norellus
+Norpin
+Nyria
+Ocampa
+Oceanus
+Ogus
+Ohniaka
+Omega
+Omekla
+Omicron Ceti
+Omicron
+Omicron Theta
+Ophiucus
+Oran'taku
+Orelius
+Orendal
+Organia
+Orias
+Orion
+Ornara
+Oshionion Prime
+Osinar
+Otar
+Pacifica
+Palgrenax
+Pandril
+Panora
+Parada
+Paragaan
+Parsion
+Peliar
+Pelleus
+Pendari
+Pendi
+Pentarus
+Pentath
+Penthara
+Penthath
+Pernaia Prime
+Persephone
+Phanos
+Platonius
+Pluto
+Pollux
+Porakas
+Portas
+Potak
+Prakal
+Praxis
+Preenos
+Prema
+Psi 2000
+Pullock
+Pyris
+Pythro
+Qo'nos
+Qu'vat
+Quadra Sigma
+Qualor
+Quarra
+Quatal Prime
+Quazulu
+Quinor
+Raatooras
+Rahm-Izad
+Rakal
+Rakella Prime
+Rakhar
+Rakosa
+Ramatis
+Ramura
+Ranuos
+Ravanar
+Razna
+Regula
+Regulak
+Regulus
+Rekag Seronia
+Relva
+Remmil
+Remus
+Rhaandarel
+Rigel
+Rinax
+Risa
+Rivos
+Rochanie
+Romulus
+Ronara Prime
+Rondac
+Rousseau
+Ruah
+Rubicun
+Rujian
+Rura Penthe
+Rutia
+Sakura Prime
+Salinia Prime
+Saltok
+Salva
+Sarona
+Sarpeidon
+Sarthong
+Saturn
+Sauria
+Scalos
+Secarus
+Selay
+Selek
+Selnia Prime
+Sentinel Minor
+Septimus
+Seros
+Setlik
+Sha Ka
+Shantil
+Sheva
+Shiralea
+Sigma Draconis
+Sigma Iotia
+Signia Minor
+Sikaris
+Simperia
+Sobras
+Solais
+Solarion
+Solosos
+Sothis
+Soukara
+Stakoron
+Stameris
+Straleb
+Styris
+Surata
+Suvin
+Syrath
+Tagra
+Tagus
+Takar
+Takara
+Talagos Prime
+Talax
+Talos
+Tamaal
+Tamaros
+Tandar Prime
+Tantalus
+Tanuga
+Tarakis
+Tarchannen
+Tarella
+Taresia
+Tarkalea
+Tarod
+Tarok
+Tarsas
+Tarsus
+Tartaras
+Tau Alpha
+Tau Ceti
+Tau Ceti Prime
+Tau Cygna
+Taurus
+Tavela Minor
+Teerza Prime
+Telfas prime
+Tellar
+Telsius Prime
+Teluridian
+Terlina
+Terosa Prime
+Terra Nova
+Tesnia
+Tessen
+Tessik Prime
+Tethys
+Thalos
+Thanatos
+Thasus
+Thelka
+Theta Cygni
+Theta
+Tiburon
+Tilonus
+Timor
+Titan
+Titus
+Tkon
+Tohvun
+Torad
+Toranius Prime
+Torga
+Torman
+Torna
+Torona
+Torros
+Tracken
+Tranome
+Trelka
+Trelkis Three
+Triacus
+Trialas
+Triannon
+Trill
+Trillius Prime
+Trinay
+Triskelion
+Troyius
+Turakus
+Turkana
+Tycho
+Typerias
+Tyrannius Prime
+Tyree
+Tyrellia
+Tyrus
+Tzenketh
+Ufandi
+Ultima Thule
+Umoth
+Unefra
+Vacca
+Vadris
+Vagra
+Valakis
+Valo
+Valt Minor
+Vandor
+Vandros
+Varala
+Vashti
+Vega
+Vega Reticuli
+Velara
+Velos
+Veloz Prime
+Vendikar
+Ventani
+Ventax
+Venus
+Verex
+Vergessen
+Veridian
+Vico
+Vilmor
+Vissia
+Volchok Prime
+Volon
+Vulcan
+Xanthras
+Xantoras
+Xarantine
+Xerxes
+Xindus
+Yadalla Prime
+Yadera
+Yadera Prime
+Yerad
+Yridia
+Zadar
+Zalkon
+Zayra
+Zenstala
+Zeon
+Zeta Alpha
+Zetar
+Zytchin
+""".strip().splitlines(keepends=False)
