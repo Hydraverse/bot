64d2c59c40ff15e92b840803efc0cca6abba9715
Phillip Sitbon <phillip.sitbon@gmail.com>
2023-02-26 10:10:59 -0800

    use logging.

--- hybot/bot/hydra/__init__.py
+++ hybot/bot/hydra/__init__.py
@@ -141,7 +141,7 @@ class HydraBot(Bot):
 
         @self.dp.message(F.text.startswith("/fiat").or_(F.text.startswith("/price")))
         async def fiat(msg: types.Message):
-            print("fiat", msg, file=sys.stderr)
+            log.warning(f"fiat command: {msg}")
             return await self.command(msg, cmd_fiat.fiat)
 
         @self.dp.message(F.text.startswith("/conf"))
